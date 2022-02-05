d5ecd5dff8050abf20c746ccc594840e4bf5a03c
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2022-02-05 15:10:52 -0800

    adding data

---
+++ src/routes/parse.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+	// import xml2json from 'xml2js';
+	let files;
+
+	$: if (files) {
+		console.log('test')
+		var file = files[0]
+		fetch('http://localhost:3000/scripts/parsedata', {
+  			method: 'POST', // or 'PUT'
+  			headers: {
+    			'Content-Type': 'application/json',
+  			},
+ 			body: JSON.stringify({data: file}),
+		})
+	}
+</script>
+
+<h1>{'Select a File'}</h1>
+<input bind:files id="avatar" name="avatar" type="file" />
