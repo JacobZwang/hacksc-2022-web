8604d752515ae52eea9e63e99e90ed0e6460277f
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2022-02-05 12:15:07 -0800

    file upload stuff

---
+++ src/routes/parce.svelte
@@ -0,0 +1,14 @@
+<script>
+	import stuff from "./script.fdx?raw"
+	import xml2json from 'xml2json'
+
+    var jsonString = xml2json.toJson(stuff);
+	var json = JSON.parse(jsonString);
+	var content = json.FinalDraft.Content.Paragraph
+
+	content.forEach((element) => console.log(element))
+	// console.log()
+ 
+</script>
+
+<h1>{"test"}</h1>
