a8a231504fbb60d01e5af62fbb1de4bc4d2b5652
JacobZwang <zwang.jacob@gmail.com>
2022-02-04 23:38:51 -0800

    start on file input

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,15 @@
-<script>
+<script lang="ts">
 	import { io } from 'socket.io-client';
 	const client = io('http://localhost:420');
 	client.onAny(function (event, data) {
 		console.log('Data', event, data);
 	});
+
+	const fileInput = document.getElementById('input') as HTMLInputElement;
+	fileInput.onchange = () => {
+		const selectedFile = fileInput.files[0];
+		console.log(selectedFile);
+	};
 </script>
 
-<h1>Header</h1>
+<input type="file" id="input" />
