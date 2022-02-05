1a3a2ebc4ea964f20834b4d9a3cc6d2bf88a092e
JacobZwang <zwang.jacob@gmail.com>
2022-02-04 23:16:52 -0800

    bare bones script

---
+++ src/routes/scripts/[script].svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	const text =
+		'Incididunt consectetur sunt exercitation adipisicing nisi dolore enim aliqua dolore exercitation. Minim ipsum amet adipisicing ut proident irure fugiat minim in aute veniam in exercitation. Reprehenderit pariatur cupidatat quis ex. Id esse adipisicing id occaecat excepteur veniam reprehenderit qui non.';
+
+	let activeWordNumber = 5;
+</script>
+
+<div>
+	{#each text.split(' ') as word, i}
+		<span class:active={i === activeWordNumber}>
+			{word}
+		</span>
+	{/each}
+</div>
+
+<style>
+	span {
+		color: rgb(104, 104, 104);
+		font-size: 30pt;
+		font-family: sans-serif;
+	}
+
+	.active {
+		color: black;
+	}
+</style>
