8247328f372d613231fc9d451fe2084395ff5004
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2022-02-05 08:27:18 -0800

    started script connecting

--- src/routes/scripts/[script].svelte
+++ src/routes/scripts/[script].svelte
@@ -1,11 +1,49 @@
+<script lang="ts" context="module">
+	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+
+	export async function load({ params }: LoadInput): Promise<LoadOutput> {
+		return {
+			props: {
+				scriptId: params.script
+			}
+		};
+	}
+</script>
+
 <script lang="ts">
 	import { io } from 'socket.io-client';
 	import temp from './_temp.txt?raw';
-
+	enum ConnectionStatus {
+		JoinedRoom,
+		Connecting,
+		Connected,
+		Disconnected,
+		Error
+	}
+	let connectionStatus = ConnectionStatus.Connecting;
+	export let scriptId;
 	// const client = io('http://localhost:420');
 	// client.onAny(function (event, data) {
 	// 	console.log('Data', event, data);
 	// });
+	const client = io('http://localhost:420');
+	client.on('connect', function () {
+		console.log('Connected');
+		client.emit('joinRoom', scriptId);
+		connectionStatus = ConnectionStatus.Connected;
+	});
+	client.on('disconnect', function () {
+		console.log('Disconnected');
+		connectionStatus = ConnectionStatus.Disconnected;
+	});
+	client.on('connect_error', function (error) {
+		console.log('Error', error);
+		connectionStatus = ConnectionStatus.Error;
+	});
+	client.on('joinRoom', function () {
+		console.log('Joined room');
+		connectionStatus = ConnectionStatus.JoinedRoom;
+	});
 
 	const text = temp;
 
@@ -18,14 +56,29 @@
 		console.log(window.scrollY);
 	}}
 />
-
-<div>
-	{#each text.split(' ') as word, i}
-		<span class:active={i === activeWordNumber}>
-			{word}
-		</span>
-	{/each}
-</div>
+{#if connectionStatus === ConnectionStatus.JoinedRoom}
+	<div>
+		{#each text.split(' ') as word, i}
+			<span class:active={i === activeWordNumber}>
+				{word}
+			</span>
+		{/each}
+	</div>
+{:else if connectionStatus === ConnectionStatus.Connected}
+<p>Joining Room #{scriptId}</p>
+{:else if connectionStatus === ConnectionStatus.Connecting}
+	<p>Connecting</p>
+{:else if connectionStatus === ConnectionStatus.Disconnected}
+	<p
+		on:click={() => {
+			client.connect();
+		}}
+	>
+		Disconnected, retry?
+	</p>
+{:else if connectionStatus === ConnectionStatus.Error}
+	<p>Error</p>
+{/if}
 
 <style>
 	div {
