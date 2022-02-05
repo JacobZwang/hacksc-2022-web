a82e6c646b8d6df0a82e88374e871950c6bdfd06
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2022-02-04 22:56:45 -0800

    format

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,9 @@
 <script>
-
-    import {io } from 'socket.io-client';
-   const client = io('http://localhost:420');
-   client.onAny(function(event,data){
-       console.log("Data",event,data);
-   });
+	import { io } from 'socket.io-client';
+	const client = io('http://localhost:420');
+	client.onAny(function (event, data) {
+		console.log('Data', event, data);
+	});
 </script>
+
 <h1>Header</h1>
