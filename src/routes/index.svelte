e3ad3a0e628605c07d407641c1fc85eea8bab246
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2022-02-06 07:45:57 -0800

    ah m push it

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,33 +1,54 @@
 <script lang="ts">
+	import { browser } from '$app/env';
+	import { goto } from '$app/navigation';
 	import { io } from 'socket.io-client';
-	const client = io('http://localhost:420');
 
-    let scrollY;
+	import { onMount } from 'svelte';
 
-    client.on('connect', () => {
-        console.log('connected');
-    });
-    client.on('disconnect', () => {
-        console.log('disconnected');
-    });
+	// import xml2json from 'xml2js';
+	let files: FileList | null;
 
-	client.onAny(function (event, data) {
-		// console.log('Data', event, data);
-	});
+	$: {
+		if (browser && files) {
+			function readFileContent(file: File) {
+				const reader = new FileReader();
+				return new Promise((resolve, reject) => {
+					reader.onload = (event) => resolve(event.target.result);
+					reader.onerror = (error) => reject(error);
+					reader.readAsText(file);
+				});
+			}
+			if (files) {
+				console.log('test', files.item(0));
+			}
+			const file = files[0];
 
-    
+			readFileContent(file).then((val) => {
+				console.log('VALL', val);
 
-	// const fileInput = document.getElementById('input') as HTMLInputElement;
-	// fileInput.onchange = () => {
-	// 	const selectedFile = fileInput.files[0];
-	// 	console.log(selectedFile);
-	// };
+				fetch('/scripts/parsedata', {
+					method: 'POST', // or 'PUT'
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({ data: val })
+				}).then(async (data) => {
+					var direct = file.name.split('.')[0];
+					const json = await data.json();
+					console.log(json);
+
+					const client = io('https://hacksc-2022-socket-ry2a5ejena-wl.a.run.app');
+					console.log('await', json, direct);
+					client.emit('post', {
+						script: json,
+						scene: direct
+					});
+					goto('/scripts/' + direct);
+				});
+			});
+		}
+	}
 </script>
-<svelte:window bind:scrollY={scrollY} on:scroll={()=>{
-    console.log("SCROLLL")
-    client.emit('scroll', {
-        scrollTop: window.scrollY,
-    });
-}}/>
-<input type="file" id="input" />
-<div style="height: 1000px"/>
+
+<h1>Select a File</h1>
+<input bind:files id="avatar" name="avatar" type="file" />
